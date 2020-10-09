207b3fb20f22e7d6c6d86ae3a113bd86ab363bb6
unknown <adarshb20@outlook.com>
2020-10-09 18:41:20 +0530

    log in!!!!!!!!!!1

---
+++ database.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+
+conn = sqlite3.connect('companies.db')
+db = conn.cursor()
+
+
+db.execute("CREATE TABLE IF NOT EXISTS 'Profiles' ('Email' varchar(100) PRIMARY KEY NOT NULL, 'Company Name' varchar(100) NOT NULL, 'Company Number' bigint NOT NULL, 'Company Address' varchar(10000) NOT NULL, 'password' text NOT NULL)")
+
